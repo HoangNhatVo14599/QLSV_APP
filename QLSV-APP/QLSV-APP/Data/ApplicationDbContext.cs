369ef3a6240ea3b8b43915504b6032b2329359c6
Nhật Võ <vohoangnhatvip1@gmail.com>
2024-03-14 11:43:14 +0700

    push

---
+++ QLSV-APP/QLSV-APP/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using QLSV_APP.Models;
+
+namespace QLSV_APP.Controllers
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Students> Students { get; set; }
+
+
+        
+    }
+}
---
+++ QLSV-APP/QLSV-APP/Models/Students.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QLSV_APP.Models
+{
+    public class Students
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string StudentCode { get; set; }
+
+        public string DateOfBirth { get; set; }
+
+        public string ExamResult { get ; set ;}
+
+        public string HomeTown { get; set; }
+        public string Description { get; set; }
+
+        public string Aspiration { get; set; }
+    }
+}
